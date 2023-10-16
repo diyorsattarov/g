bd395590d284c2d42ba671f6dfb02a7e19c35b0a
diyooor <diyorsattarov7@gmail.com>
2023-10-15 21:50:22 -0500

    Reorganized timeline so that its ordered ascending, most recent update->latest update

--- src/components/Timeline/Timeline.jsx
+++ src/components/Timeline/Timeline.jsx
@@ -6,12 +6,165 @@ import video1 from "./../../media/videos/oct7/video1.mp4";
 import video2 from "./../../media/videos/oct7/video2.mp4";
 import video3 from "./../../media/videos/oct15/video1.mp4";
 import video4 from "./../../media/videos/oct15/video2.mp4";
-import image1 from "./../../media/images/oct7/image1.png"
+import image1 from "./../../media/images/oct7/image1.png";
 
 export default function DefaultTimeline({ darkMode }) {
 	return (
 		<div className={`timeline-container ${darkMode ? "dark" : ""}`}>
 			<Timeline className={`custom-timeline ${darkMode ? "dark" : ""}`}>
+				<Timeline.Item>
+					<Timeline.Point />
+					<Timeline.Content>
+						<Timeline.Time>October 16, 2023</Timeline.Time>
+						<Timeline.Title>Gaza Genocide Day 10</Timeline.Title>
+						<Timeline.Body>
+							<p>Content body</p>
+						</Timeline.Body>
+						<Button color="gray">
+							<p>Learn More</p>
+							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
+						</Button>
+					</Timeline.Content>
+				</Timeline.Item>
+
+				<Timeline.Item>
+					<Timeline.Point />
+					<Timeline.Content>
+						<Timeline.Time>October 15, 2023</Timeline.Time>
+						<Timeline.Title>Gaza Genocide Day 9</Timeline.Title>
+						<Timeline.Body>
+							<div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+								<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+									<video class="w-full" controls>
+										<source src={video3} type="video/mp4" />
+										Your browser does not support the video tag.
+									</video>
+								</p>
+								<p>
+									The IDF mistakenly kills a settler in Ofakim, near the Gaza
+									Strip; After they thought he was a Palestinian fighter
+								</p>
+								<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+									<video class="w-full" controls>
+										<source src={video4} type="video/mp4" />
+										Your browser does not support the video tag.
+									</video>
+								</p>
+								<p>The extent of the destruction in Gaza.</p>
+							</div>
+						</Timeline.Body>
+						<Button color="gray">
+							<p>Learn More</p>
+							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
+						</Button>
+					</Timeline.Content>
+				</Timeline.Item>
+
+				<Timeline.Item>
+					<Timeline.Point />
+					<Timeline.Content>
+						<Timeline.Time>October 14, 2023</Timeline.Time>
+						<Timeline.Title>Gaza Genocide Day 8</Timeline.Title>
+						<Timeline.Body>
+							<p>Content body</p>
+						</Timeline.Body>
+						<Button color="gray">
+							<p>Learn More</p>
+							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
+						</Button>
+					</Timeline.Content>
+				</Timeline.Item>
+
+				<Timeline.Item>
+					<Timeline.Point />
+					<Timeline.Content>
+						<Timeline.Time>October 13, 2023</Timeline.Time>
+						<Timeline.Title>Gaza Genocide Day 7</Timeline.Title>
+						<Timeline.Body>
+							<p>Content body</p>
+						</Timeline.Body>
+						<Button color="gray">
+							<p>Learn More</p>
+							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
+						</Button>
+					</Timeline.Content>
+				</Timeline.Item>
+
+				<Timeline.Item>
+					<Timeline.Point />
+					<Timeline.Content>
+						<Timeline.Time>October 12, 2023</Timeline.Time>
+						<Timeline.Title>Gaza Genocide Day 6</Timeline.Title>
+						<Timeline.Body>
+							<p>Content body</p>
+						</Timeline.Body>
+						<Button color="gray">
+							<p>Learn More</p>
+							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
+						</Button>
+					</Timeline.Content>
+				</Timeline.Item>
+
+				<Timeline.Item>
+					<Timeline.Point />
+					<Timeline.Content>
+						<Timeline.Time>October 11, 2023</Timeline.Time>
+						<Timeline.Title>Gaza Genocide Day 5</Timeline.Title>
+						<Timeline.Body>
+							<p>Content body</p>
+						</Timeline.Body>
+						<Button color="gray">
+							<p>Learn More</p>
+							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
+						</Button>
+					</Timeline.Content>
+				</Timeline.Item>
+
+				<Timeline.Item>
+					<Timeline.Point />
+					<Timeline.Content>
+						<Timeline.Time>October 10, 2023</Timeline.Time>
+						<Timeline.Title>Gaza Genocide Day 4</Timeline.Title>
+						<Timeline.Body>
+							<p>Content body</p>
+						</Timeline.Body>
+						<Button color="gray">
+							<p>Learn More</p>
+							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
+						</Button>
+					</Timeline.Content>
+				</Timeline.Item>
+
+				<Timeline.Item>
+					<Timeline.Point />
+					<Timeline.Content>
+						<Timeline.Time>October 9, 2023</Timeline.Time>
+						<Timeline.Title>Gaza Genocide Day 3</Timeline.Title>
+						<Timeline.Body>
+							<p>Content body</p>
+						</Timeline.Body>
+						<Button color="gray">
+							<p>Learn More</p>
+							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
+						</Button>
+					</Timeline.Content>
+				</Timeline.Item>
+
+				<Timeline.Item>
+					<Timeline.Point />
+					<Timeline.Content>
+						<Timeline.Time>October 8, 2023</Timeline.Time>
+						<Timeline.Title>Gaza Genocide Day 2</Timeline.Title>
+						<Timeline.Body>
+							<p>Content body</p>
+						</Timeline.Body>
+						<Button color="gray">
+							<p>Learn More</p>
+							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
+						</Button>
+					</Timeline.Content>
+				</Timeline.Item>
+
 				<Timeline.Item>
 					<Timeline.Point />
 					<Timeline.Content>
@@ -41,24 +194,27 @@ export default function DefaultTimeline({ darkMode }) {
 											wing of Hamas, said.{" "}
 										</p>
 									</blockquote>
-                  <img class="rounded-t-lg" src={image1} alt="" />
+									<img class="rounded-t-lg" src={image1} alt="" />
 								</p>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-                  04:40 GMT – Dawn infiltration
+								<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+									04:40 GMT – Dawn infiltration
 									<blockquote class="p-4 my-4 border-l-4 border-gray-300 bg-gray-50 dark:border-gray-500 dark:bg-gray-800">
 										<p class="text-xl italic font-medium leading-relaxed text-gray-900 dark:text-white">
-                      The rocket attack served as cover for an unprecedented multipronged infiltration of fighters with the Israeli military saying at 7:40am (04:40 GMT) that Palestinian fighters had crossed into Israel.
+											The rocket attack served as cover for an unprecedented
+											multipronged infiltration of fighters with the Israeli
+											military saying at 7:40am (04:40 GMT) that Palestinian
+											fighters had crossed into Israel.
 										</p>
 									</blockquote>
 								</p>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+								<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
 									<video class="w-full" controls>
 										<source src={video1} type="video/mp4" />
 										Your browser does not support the video tag.
 									</video>
 								</p>
-                <th />
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
+								<th />
+								<p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
 									<video class="w-full" controls>
 										<source src={video2} type="video/mp4" />
 										Your browser does not support the video tag.
@@ -66,140 +222,6 @@ export default function DefaultTimeline({ darkMode }) {
 								</p>
 							</div>
 							<th />
-						</Timeline.Body>
-						<Button color="gray">
-							<p>Learn More</p>
-							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
-						</Button>
-					</Timeline.Content>
-				</Timeline.Item>
-
-				<Timeline.Item>
-					<Timeline.Point />
-					<Timeline.Content>
-						<Timeline.Time>October 8, 2023</Timeline.Time>
-						<Timeline.Title>Gaza Genocide Day 2</Timeline.Title>
-						<Timeline.Body>
-							<p>Content body</p>
-						</Timeline.Body>
-						<Button color="gray">
-							<p>Learn More</p>
-							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
-						</Button>
-					</Timeline.Content>
-				</Timeline.Item>
-				<Timeline.Item>
-					<Timeline.Point />
-					<Timeline.Content>
-						<Timeline.Time>October 9, 2023</Timeline.Time>
-						<Timeline.Title>Gaza Genocide Day 3</Timeline.Title>
-						<Timeline.Body>
-							<p>Content body</p>
-						</Timeline.Body>
-						<Button color="gray">
-							<p>Learn More</p>
-							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
-						</Button>
-					</Timeline.Content>
-				</Timeline.Item>
-
-				<Timeline.Item>
-					<Timeline.Point />
-					<Timeline.Content>
-						<Timeline.Time>October 10, 2023</Timeline.Time>
-						<Timeline.Title>Gaza Genocide Day 4</Timeline.Title>
-						<Timeline.Body>
-							<p>Content body</p>
-						</Timeline.Body>
-						<Button color="gray">
-							<p>Learn More</p>
-							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
-						</Button>
-					</Timeline.Content>
-				</Timeline.Item>
-
-				<Timeline.Item>
-					<Timeline.Point />
-					<Timeline.Content>
-						<Timeline.Time>October 11, 2023</Timeline.Time>
-						<Timeline.Title>Gaza Genocide Day 5</Timeline.Title>
-						<Timeline.Body>
-							<p>Content body</p>
-						</Timeline.Body>
-						<Button color="gray">
-							<p>Learn More</p>
-							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
-						</Button>
-					</Timeline.Content>
-				</Timeline.Item>
-
-				<Timeline.Item>
-					<Timeline.Point />
-					<Timeline.Content>
-						<Timeline.Time>October 12, 2023</Timeline.Time>
-						<Timeline.Title>Gaza Genocide Day 6</Timeline.Title>
-						<Timeline.Body>
-							<p>Content body</p>
-						</Timeline.Body>
-						<Button color="gray">
-							<p>Learn More</p>
-							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
-						</Button>
-					</Timeline.Content>
-				</Timeline.Item>
-
-				<Timeline.Item>
-					<Timeline.Point />
-					<Timeline.Content>
-						<Timeline.Time>October 13, 2023</Timeline.Time>
-						<Timeline.Title>Gaza Genocide Day 7</Timeline.Title>
-						<Timeline.Body>
-							<p>Content body</p>
-						</Timeline.Body>
-						<Button color="gray">
-							<p>Learn More</p>
-							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
-						</Button>
-					</Timeline.Content>
-				</Timeline.Item>
-
-				<Timeline.Item>
-					<Timeline.Point />
-					<Timeline.Content>
-						<Timeline.Time>October 14, 2023</Timeline.Time>
-						<Timeline.Title>Gaza Genocide Day 8</Timeline.Title>
-						<Timeline.Body>
-							<p>Content body</p>
-						</Timeline.Body>
-						<Button color="gray">
-							<p>Learn More</p>
-							<HiArrowNarrowRight className="ml-2 h-3 w-3" />
-						</Button>
-					</Timeline.Content>
-				</Timeline.Item>
-
-				<Timeline.Item>
-					<Timeline.Point />
-					<Timeline.Content>
-						<Timeline.Time>October 15, 2023</Timeline.Time>
-						<Timeline.Title>Gaza Genocide Day 9</Timeline.Title>
-						<Timeline.Body>
-            <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-              <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-									<video class="w-full" controls>
-										<source src={video3} type="video/mp4" />
-										Your browser does not support the video tag.
-									</video>
-								</p>
-                <p>The IDF mistakenly kills a settler in Ofakim, near the Gaza Strip; After they thought he was a Palestinian fighter</p>
-                <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">
-									<video class="w-full" controls>
-										<source src={video4} type="video/mp4" />
-										Your browser does not support the video tag.
-									</video>
-								</p>
-                <p>The extent of the destruction in Gaza.</p>
-                </div>
 						</Timeline.Body>
 						<Button color="gray">
 							<p>Learn More</p>
